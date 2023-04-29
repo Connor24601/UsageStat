bf400b8332988712f2f7334f930a50316cab2a72
Blake Johnson <blakeconnorjohnson@gmail.com>
2023-04-29 15:36:35 -0500

    initial commit

--- dataHandler.py
+++ dataHandler.py
@@ -1,6 +1,25 @@
 import csv
-
+import time
+import systemHandler
 
 '''This function writes data into the CSV'''
-def writeData(fileStr, data):
+def writeData(data):
 	# getter
+	filePath = systemHandler.getFile()
+	with open(filePath,"w") as doc:
+		cdoc = csv.reader(doc)
+
+
+def format(isStartup):
+	# test
+
+time.ctime()
+
+def checkData():
+	filePath = systemHandler.getFile()
+	with open(filePath, "r") as doc:
+		# TODO: add actual parsing here
+		if (last line contains uptime):
+			main.promptUser()
+		
+
--- systemHandler.py
+++ systemHandler.py
@@ -1,3 +1,23 @@
 import sys
 import os
 
+filePath = ""
+
+'''findFile gets the computer name and finds the data file located with it'''
+def getFile():
+	if filePath != "":
+		return filePath
+	"""
+	get pc name
+	check ./data/pcnameHASH.csv
+	catch(filenotfoundexception):
+		createFile()
+	filePath = file.path
+
+	"""
+
+'''Creates a file if a data file does not exist'''
+def createFile():
+	'''
+	os/sys file creation w hash
+	'''
